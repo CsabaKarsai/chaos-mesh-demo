d7b3b97e902a9bcfdd69b99cfb6baae6b3bb917c
csabakarsai <csaba@karsai.info>
2022-09-23 10:19:19 +0200

    added ks manifests for db and app

--- src/Main.java
+++ src/Main.java
@@ -15,18 +15,21 @@ public class Main{
                 e.printStackTrace();
             }
             try {
-                Connection conn = DriverManager.getConnection("jdbc:mysql://10.97.96.180:3306/", "root", "root");
+                Connection conn = DriverManager.getConnection("jdbc:mysql://db:3306/", "root", "");
                 if (conn != null) {
                     connectionEstablished = true;
-                    System.out.println("Connection to db successful!");
+                    System.out.println("Connection to k8s db successful!");
                     conn.createStatement().execute("SET GLOBAL max_connections = 2000;");
+                    conn.createStatement().execute("CREATE DATABASE IF NOT EXISTS db;");
+                    System.out.println("database creation successful!");
                     conn.createStatement().execute("USE db;");
                     conn.createStatement().execute("CREATE TABLE IF NOT EXISTS time(seconds int, PRIMARY KEY (seconds));");
-                    conn.createStatement().executeUpdate("TRUNCATE TABLE time");
+                    System.out.println("database creation successful!");
+                    //conn.createStatement().executeUpdate("TRUNCATE TABLE time");
                 }
                 conn.close();
             } catch (Exception e) {
-                System.err.println("Got an exception!");
+                System.err.println("Got an exception in conn part!");
                 System.err.println(e.getMessage());
             }
             if (!connectionEstablished) {
@@ -35,6 +38,17 @@ public class Main{
         }
 
         System.out.println("Got through the connection code!");
+        int counter = 0;
+        while (true) {
+            try {
+                Thread.sleep(1000);
+                counter += 1;
+                System.out.println("counter: " + counter);
+            } catch (InterruptedException e) {
+                System.out.println("Interrupted!");
+                e.printStackTrace();
+            }
+        }
 
     }
 
